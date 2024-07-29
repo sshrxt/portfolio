fc5f0ce8a8cd199aae511cce9daa5adc4aa54180
Shrut <shrutpatel20@gmail.com>
2024-07-29 18:11:38 -0500

    add approach section

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -17,6 +19,8 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
+        <Footer/>
       </div>
     </main>
   );
