0a3177f5812cde7c29fea5807ef46c28715b1814
Shrut <shrutpatel20@gmail.com>
2024-07-30 15:25:57 -0500

    revamp work experience and add technology section

---
+++ components/ui/ExperienceCard.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+
+const ExperienceCard: any = ({ experience }: { experience: any }) => {
+    console.log(experience)
+  return (
+    <VerticalTimelineElement
+      visible={true}
+      contentStyle={{ background: "#1d1836", color: "#fff" }}
+      contentArrowStyle={{ borderRight: "7px solid #232631" }}
+      date={experience.date}
+      iconStyle={{ background: experience.iconBg }}
+      icon={
+        <div className="flex justify-center items-center w-full h-full">
+          <img
+            src={experience.icon.src}
+            alt={experience.company_name}
+            className="w-[60%] h-[60%] object-contain"
+          />
+        </div>
+      }
+    >
+      <div>
+        <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+        <p
+          className='text-white-200 text-[16px] font-semibold'
+          style={{ margin: 0 }}
+        >
+          {experience.company_name}
+        </p>
+      </div>
+
+      <ul className='mt-5 list-disc ml-5 space-y-2'>
+        {experience.points.map((point:any, index: number) => (
+          <li
+            key={`experience-point-${index}`}
+            className='text-white-100 text-[14px] pl-1 tracking-wider'
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
+  )
+};
+
+export default ExperienceCard;
