5cf4630b09559a02867fff67897cb461a7640f4c
Shrut <shrutpatel20@gmail.com>
2024-07-31 13:34:59 -0500

    removed items from navbar

--- data/index.ts
+++ data/index.ts
@@ -46,8 +46,6 @@ export const navItems = [
     { name: "About", link: "#about" },
     { name: "Projects", link: "#projects" },
     { name: "Experience", link: "#experience" },
-    { name: "Testimonials", link: "#testimonials" },
-    { name: "My Approach", link: "#approach" },
     { name: "Contact", link: "#contact" },
   ];
   
