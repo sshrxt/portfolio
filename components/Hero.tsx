103872f3a8d167b2c14700724be73c0ba08fe26b
Shrut <shrutpatel20@gmail.com>
2024-07-31 14:09:30 -0500

    fix positioning of hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-20">
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
